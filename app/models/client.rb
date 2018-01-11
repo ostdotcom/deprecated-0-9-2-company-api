745b2654860dc62e9a3d8ea0e2431d55ddfe452e
kedarchandrayan <kedar@pepo.com>
2018-01-11 12:55:15 +0530

    client enum and bitwise column.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,8 @@
 class Client < EstablishCompanyClientDbConnection
+
+  enum status: {
+    GlobalConstant::User.active_status => 1,
+    GlobalConstant::User.inactive_status => 2
+  }
+
 end
---
+++ lib/global_constant/client.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module GlobalConstant
+
+  class Client
+
+    class << self
+
+      ### Status Start ###
+
+      def active_status
+        'active'
+      end
+
+      def inactive_status
+        'inactive'
+      end
+
+      ### Status End ###
+
+    end
+
+  end
+
+end
