ebb15c5468ca40fe471226b1044a7f2ab65936aa
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-02-14 00:29:45 +0530

    fix

--- db/migrate/20180213101658_create_client_branded_tokens.rb
+++ db/migrate/20180213101658_create_client_branded_tokens.rb
@@ -1,26 +1,26 @@
-class CreateSassClientTokens < DbMigrationConnection
+class CreateClientBrandedTokens < DbMigrationConnection
   def up
     run_migration_for_db(EstablishSaasClientEconomyDbConnection) do
 
-      create_table :client_tokens do |t|
+      create_table :client_branded_tokens do |t|
         t.column :client_id, :integer, null: false
+        t.column :reserve_managed_address_id, :integer
         t.column :symbol, :string
-        t.column :reserve_managed_address_id, :string
         t.column :token_erc20_address, :string
         t.column :token_uuid, :string
         t.column :conversion_rate, :decimal, null: true, precision: 12, scale: 6
         t.timestamps
       end
 
-      add_index :client_tokens, :client_id, name: 'index_client_id'
-      add_index :client_tokens, :symbol, name: 'uniq_symbol'
+      add_index :client_branded_tokens, :client_id, name: 'index_client_id'
+      add_index :client_branded_tokens, :symbol, name: 'uniq_symbol'
     end
   end
 
   def down
     run_migration_for_db(EstablishSaasClientEconomyDbConnection) do
 
-      drop_table :client_tokens
+      drop_table :client_branded_tokens
 
     end
   end
