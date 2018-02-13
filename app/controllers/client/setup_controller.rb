4ff8d36927b7c8730f777215448d1afa560e11e4
pankajkeswani <pankaj@pepo.com>
2018-02-13 16:52:51 +0530

    Route for getting test ost

--- app/controllers/client/setup_controller.rb
+++ app/controllers/client/setup_controller.rb
@@ -14,4 +14,18 @@ class Client::SetupController < Client::BaseController
 
   end
 
+  # Get Test OST
+  #
+  # * Author: Pankaj
+  # * Date: 12/02/2018
+  # * Reviewed By:
+  #
+  def get_test_ost
+
+    service_response = ClientManagement::GetTestOst.new(params).perform
+
+    render_api_response(service_response)
+
+  end
+
 end
