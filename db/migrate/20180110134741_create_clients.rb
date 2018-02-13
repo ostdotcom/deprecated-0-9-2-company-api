9e643be7097dadec682a7034e028e4d73cbedf30
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-02-13 22:18:29 +0530

    migrations changes for clients table

--- db/migrate/20180110134741_create_clients.rb
+++ db/migrate/20180110134741_create_clients.rb
@@ -1,16 +1,24 @@
 class CreateClients < DbMigrationConnection
 
-  def change
+  def up
 
     run_migration_for_db(EstablishCompanyClientDbConnection) do
 
       create_table :clients do |t|
-        t.column :properties, :tinyint, null: true
-        t.column :info_salt, :blob, null: false
+        t.column :client_name, :string, null: true
+        t.column :client_website, :string, null: true
         t.column :status, :tinyint, null: false
         t.timestamps
       end
 
+    end
+
+  end
+
+  def down
+
+    run_migration_for_db(EstablishCompanyClientDbConnection) do
+      drop_table :clients
     end
 
   end
