e10bf5dcca2eca788953f59d71d1da717a55f218
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-02-15 08:32:54 +0530

    fixes to make signup work !!

--- lib/email_token_encryptor.rb
+++ lib/email_token_encryptor.rb
@@ -1,20 +1,45 @@
 class EmailTokenEncryptor
 
+  include Util::ResultHelper
+
   def initialize(key)
     @key = key
   end
 
   def encrypt(string)
-    client.encrypt_and_sign(string)
+    handle_exeption('encrypt') do
+      ciphertext_blob = client.encrypt_and_sign(string)
+      success_with_data(ciphertext_blob: ciphertext_blob)
+    end
   end
 
   def decrypt(encrypted_text)
-    client.decrypt_and_verify(encrypted_text)
+    handle_exeption('decrypt') do
+      plaintext = client.decrypt_and_verify(encrypted_text)
+      success_with_data(plaintext: plaintext)
+    end
   end
 
   private
 
   def client
-    ActiveSupport::EmailTokenEncryptor.new(GlobalConstant::SecretEncryptor.email_tokens_key)
+    ActiveSupport::MessageEncryptor.new(GlobalConstant::SecretEncryptor.email_tokens_key)
   end
+
+  def handle_exeption(task)
+    begin
+      fail 'no code to execute' unless block_given?
+      yield
+    rescue => e
+      exception_with_data(
+          e,
+          'etc_1',
+          "EmailTokenEncryptor could not perform task: #{task}",
+          'Something Went Wrong.',
+          GlobalConstant::ErrorAction.default,
+          {}
+      )
+    end
+  end
+
 end
