8b84284a904ab572e4f3a91c56ca2990cd10641d
aman_barbaria <aman@pepo.com>
2018-03-03 20:39:00 +0530

    transaction page api console

--- app/services/economy/transaction_kind/list.rb
+++ app/services/economy/transaction_kind/list.rb
@@ -66,31 +66,34 @@ module Economy
       #
       def fetch_kinds
 
-        return success unless is_xhr_request?
-
         r = instantiate_ost_sdk
         return r unless r.success?
 
-        r = @ost_sdk_obj.list({})
-        return r unless r.success?
+        if is_xhr_request?
 
-        @api_response_data = r.data
+          r = @ost_sdk_obj.list({})
+          return r unless r.success?
 
-        api_spec_service_response = @ost_spec_sdk_obj.create(api_spec_params)
+          @api_response_data = r.data
 
-        return error_with_data(
-            'e_tk_l_fk_1',
-            "Coundn't fetch api Spec for transaction kind create",
-            'Something Went Wrong.',
-            GlobalConstant::ErrorAction.default,
-            {}
-        ) unless api_spec_service_response.success?
+        else
 
-        @api_response_data[:api_console_data] = {
-            transaction_kind:{
-                create: api_spec_service_response.data
-            }
-        }
+          api_spec_service_response = @ost_spec_sdk_obj.create(api_spec_params)
+
+          return error_with_data(
+              'e_tk_l_fk_1',
+              "Coundn't fetch api Spec for transaction kind create",
+              'Something Went Wrong.',
+              GlobalConstant::ErrorAction.default,
+              {}
+          ) unless api_spec_service_response.success?
+
+          @api_response_data[:api_console_data] = {
+              transaction_kind: {
+                  create: api_spec_service_response.data
+              }
+          }
+        end
 
         success
 
