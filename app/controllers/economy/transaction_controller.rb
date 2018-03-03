b9bde19594efa72e1a1b862a246b84fc92efeef7
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-04 00:05:12 +0530

    simulator API console data

--- app/controllers/economy/transaction_controller.rb
+++ app/controllers/economy/transaction_controller.rb
@@ -29,7 +29,6 @@ class Economy::TransactionController < Economy::BaseController
   # * Reviewed By:
   #
   def fetch_detail
-    params[:transaction_uuids] = [params[:transaction_uuid]]
     service_response = Economy::Transaction::FetchHistory.new(params).perform
     render_api_response(service_response)
   end
