f6c758e1d2044486b56ba01d5278c8feb6423817
pankajkeswani <pankaj@pepo.com>
2018-03-04 21:35:10 +0530

    Simulator transactions pagination

--- app/services/economy/transaction/fetch_history.rb
+++ app/services/economy/transaction/fetch_history.rb
@@ -23,7 +23,7 @@ module Economy
         @client_token_id = @params[:client_token_id]
         @page_no = @params[:page_no]
 
-        @page_size = 10
+        @page_size = 25
 
         @client_token = nil
         @saas_api_response_data = nil
@@ -40,167 +40,6 @@ module Economy
       #
       def perform
 
-        dummy_response = {
-
-            economy_users: {
-                1 => {
-                    id: 1,
-                    name: "company User",
-                    uuid: "a1",
-                    total_airdropped_tokens: "active",
-                    token_balance: 1000
-                },
-                2 => {
-                    id: 2,
-                    name: "Aman User1",
-                    uuid: "a2",
-                    total_airdropped_tokens: "active",
-                    token_balance: 100
-                },
-                3 => {
-                    id: 3,
-                    name: "Rachin User3",
-                    uuid: "a3",
-                    total_airdropped_tokens: "active",
-                    token_balance: 10
-                }
-
-            },
-
-            client_tokens: {
-                1 => {
-                    id: 1,
-                    client_id: 1,
-                    name: "TakTakTak",
-                    symbol: "T3K",
-                    symbol_icon: '',
-                    status: "active",
-                    conversion_factor: 3.003,
-                    setup_steps: [
-                        "token_worth_in_usd",
-                        "configure_transactions",
-                        "propose_initiated",
-                        "propose_done",
-                        "registered_on_uc",
-                        "registered_on_vc",
-                        "received_test_ost"
-                    ]
-                }
-
-            },
-
-            transaction_types: {
-                1 => {
-                    id: 1,
-                    name: "Upvote",
-                    kind: "user_to_user",
-                    currency_type: "BT",
-                    currency_value: "10",
-                    commission_percent: "0.000",
-                    status: "active"
-                },
-                2 => {
-                    id: 2,
-                    name: "Service Charge",
-                    kind: "user_to_company",
-                    currency_type: "BT",
-                    currency_value: "30",
-                    commission_percent: "0.000",
-                    status: "active"
-                },
-                3 => {
-                    id: 3,
-                    name: "Like",
-                    kind: "user_to_user",
-                    currency_type: "USD",
-                    currency_value: "20",
-                    commission_percent: "10.000",
-                    status: "active"
-                },
-            },
-
-            result_type: "transactions",
-
-            transactions: [
-                {
-                    id: 'a2-a3',
-                    transaction_uuid: 'a2-a3',
-                    status: 'active',
-                    transaction_hash: "uussyyuuwww-#{Time.now.to_i}",
-                    uts: "#{Time.now.to_i}",
-                    from_user_id: 2,
-                    to_user_id: 3,
-                    transaction_type_id: 1,
-                    client_token_id: 1,
-                    currency_value: 10,
-                    gas_value: 0.011,
-                    block_number: 1,
-                    block_timestamp: "#{Time.now.to_i}",
-                    transfers: [
-                        {
-                            from_user_id: 2,
-                            to_user_id: 3,
-                            type: 'transfer',
-                            currency_value: 10
-                        }
-                    ]
-                },
-                {
-                    id: 'a2-a1',
-                    transaction_uuid: 'a2-a1',
-                    status: 'active',
-                    transaction_hash: "uussyyuuwww-#{Time.now.to_i}",
-                    uts: "#{Time.now.to_i}",
-                    from_user_id: 2,
-                    to_user_id: 1,
-                    transaction_type_id: 2,
-                    client_token_id: 1,
-                    currency_value: 30,
-                    gas_value: 0.021,
-                    block_number: 2,
-                    block_timestamp: "#{Time.now.to_i}",
-                    transfers: [
-                        {
-                            from_user_id: 2,
-                            to_user_id: 1,
-                            type: 'transfer',
-                            currency_value: 30
-                        }
-                    ]
-                },
-                {
-                    id: 'a3-a2',
-                    transaction_uuid: 'a3-a2',
-                    status: 'pending',
-                    transaction_hash: nil,
-                    uts: "#{Time.now.to_i}",
-                    from_user_id: 3,
-                    to_user_id: 2,
-                    transaction_type_id: 3,
-                    currency_value: nil,
-                    gas_value: nil,
-                    block_number: nil,
-                    block_timestamp: nil,
-                    transfers: []
-                }
-            ],
-            oracle_price_points: {
-                ost: {
-                    USD: 0.3
-                }
-            },
-            meta: {
-                next_page_payload: {
-
-                }
-            }
-
-        }
-
-
-        # return success_with_data(dummy_response)
-
-
         r = validate_and_sanitize
         return r unless r.success?
 
@@ -216,10 +55,14 @@ module Economy
           return r unless r.success?
         end
 
-        return success if @transaction_uuids.blank?
+        return success_with_data({meta: {next_page_payload: {}}}) if @transaction_uuids.blank?
 
         r = fetch_transaction_data_from_saas
         return r unless r.success?
+
+        @saas_api_response_data[:meta] = {
+            next_page_payload: {page_no: @page_no + 1}
+        } if @transaction_uuids.count >= @page_size
 
         return success_with_data(@saas_api_response_data)
 
