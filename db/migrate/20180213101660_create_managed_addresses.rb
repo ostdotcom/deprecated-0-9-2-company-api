f0c355f9b6f17012bcb867492b216ef9ba8c6a81
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-07 19:33:58 +0530

    typo fix

--- db/migrate/20180213101660_create_managed_addresses.rb
+++ db/migrate/20180213101660_create_managed_addresses.rb
@@ -16,7 +16,7 @@ class CreateManagedAddresses < DbMigrationConnection
       end
 
       add_index :managed_addresses, [:client_id, :uuid], name: 'uk_1', unique: true
-      add_index :managed_addresses, [:managed_address_stalt_id], name: 'uk_2', unique: true
+      add_index :managed_addresses, [:managed_address_salt_id], name: 'uk_2', unique: true
       add_index :managed_addresses, [:ethereum_address], name: 'uk_3', unique: true
 
       execute ("ALTER TABLE managed_addresses AUTO_INCREMENT = 70000")
