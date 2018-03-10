d04c4b86fee918f900ce1db9ca1a37c09d01493c
santhu210 <santhosh.nitdgp2015@gmail.com>
2018-03-10 17:14:18 +0530

    changed secret key to sync with web

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -1,5 +1,5 @@
 #Note this secret has to be same for comapny-web & comapny-api in order to make CSRF work
-export COMPANY_SECRET_KEY_BASE='a8e9609c826bfa8141ed299e37d73f150134d610260d73922c06969e8c0c50036d2d44f241ef0b530992ad580fdb78cca9a21fbccdd067ad81cace6ab18d8ebf'
+export COMPANY_SECRET_KEY_BASE='b1f0ff90cd692556f9740a8e609f88f2f4fc15d9dda9035445a7577c3f94936eaae91a0793c4ad5500314fe5a526a3b3f7c7c71c303f883d903df138783a8225'
 
 # Database details
 export CA_DEFAULT_DB_HOST=127.0.0.1
