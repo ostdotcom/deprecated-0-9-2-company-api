bd32bcb7f4ecaa4aaaa9e187865328017800e3be
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-13 02:52:38 +0530

    bug fix

--- lib/explorer_api/top_users_graph.rb
+++ lib/explorer_api/top_users_graph.rb
@@ -26,31 +26,41 @@ module ExplorerApi
     # @return [Result::Base]
     #
     def perform(params)
+
       client_token = CacheManagement::ClientTokenSecure.new([params[:client_token_id]]).fetch[params[:client_token_id]]
+
       response = send_request_of_type(
           'get',
           (GlobalConstant::ExplorerApi.top_users_graph_path % [GlobalConstant::ExplorerApi.chain_id, client_token[:token_erc20_address]])
       )
+
       result_type = response.data[:result_type]
+
       if response.data[result_type].present?
         users = response.data[result_type]
         ethereum_addresses = users.map{|x| x['address']}
         resp = SaasApi::FetchClientUsersDetails.new().perform({ethereum_addresses: ethereum_addresses,
                                                         client_id: params[:client_id]})
+
+        downcased_data = {}
+        resp.data.each do |k,v|
+          downcased_data[k.downcase] = v
+        end
+
         return resp unless resp.success?
 
         new_response = []
-        not_allowed_user_uuids = [client_token[:reserve_uuid], client_token[:worker_uuid], client_token[:airdrop_holder_uuid]]
-        not_allowed_addresses = [client_token[:token_erc20_address], client_token[:airdrop_contract_address]]
+        not_allowed_user_uuids = [client_token[:reserve_uuid], client_token[:worker_uuid], client_token[:airdrop_holder_uuid]].map{|k| k.downcase}
+        not_allowed_addresses = [client_token[:token_erc20_address], client_token[:airdrop_contract_address]].map{|k| k.downcase}
         users.each do |user_data|
-          eth_address = user_data['address']
+          eth_address = user_data['address'].downcase
           next if not_allowed_addresses.include?(eth_address)
           user_data['name'] = ''
-          if resp.data.include?(eth_address)
-            next if not_allowed_user_uuids.include?(resp.data[eth_address]['uuid'])
-            user_data['name'] = resp.data[eth_address]['name']
-            user_data['tokens'] = resp.data[eth_address]['token_balance']
-            user_data['tokens_earned'] = (user_data['tokens_earned'].to_f + resp.data[eth_address]['balance_airdrop_amount'].to_f).to_s
+          if downcased_data.include?(eth_address)
+            next if not_allowed_user_uuids.include?(downcased_data[eth_address]['uuid'])
+            user_data['name'] = downcased_data[eth_address]['name']
+            user_data['tokens'] = downcased_data[eth_address]['token_balance']
+            user_data['tokens_earned'] = (user_data['tokens_earned'].to_f + downcased_data[eth_address]['balance_airdrop_amount'].to_f).to_s
             new_response << user_data
           end
         end
@@ -58,6 +68,7 @@ module ExplorerApi
       end
 
       response
+
     end
 
   end
