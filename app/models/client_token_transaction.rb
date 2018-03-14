8ce9a4cbc4d2ef4b2c730f9a7cc3fd7a821f8ffc
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-14 15:15:18 +0530

    optimized logic to send prioritize company tx flag

--- app/models/client_token_transaction.rb
+++ app/models/client_token_transaction.rb
@@ -7,7 +7,7 @@ class ClientTokenTransaction < EstablishCompanyClientEconomyDbConnection
     prioritize_tx_flags = CacheManagement::ClientPrioritizeTxFlag.new([client_token_id]).fetch[client_token_id]
 
     # if this flag is true, no need to flush. as this will not change
-    if prioritize_tx_flags.blank? || !prioritize_tx_flags[:company_to_user]
+    if prioritize_tx_flags.blank? || prioritize_tx_flags[:company_to_user]
       CacheManagement::ClientPrioritizeTxFlag.new([client_token_id]).clear
     end
 
