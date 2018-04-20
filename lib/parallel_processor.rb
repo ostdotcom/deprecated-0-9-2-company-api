3dd48454fa9bf2a3b141179719127a544dc859a1
Alpesh <alpesh@pepo.com>
2018-04-20 13:52:25 +0530

    failed count..

--- lib/parallel_processor.rb
+++ lib/parallel_processor.rb
@@ -30,10 +30,11 @@ class ParallelProcessor
   def perform
 
     batch_no = 1
+    failed_count = 0
 
     @methods_to_process.keys.each_slice(max_allowed_threads) do |batched_keys|
 
-      puts "starting batch_no: #{batch_no}"
+      puts "starting batch_no: #{batch_no} having #{batched_keys.length} transactions"
 
       threads = []
 
@@ -41,6 +42,10 @@ class ParallelProcessor
         batched_keys.each do |key|
           thread = Thread.new {
             @response[key] = @methods_to_process[key].call
+            unless @response[key].success?
+              puts @response[key].inspect
+              failed_count += 1
+            end
           }
           threads.push(thread)
         end
@@ -49,8 +54,10 @@ class ParallelProcessor
       threads.each { |thread| thread.join } # make others wait for the one taking time
 
       puts "batch_no: #{batch_no} took #{@time.to_s}"
+      batch_no += 1
 
     end
+    puts "failed_count---#{failed_count}"
 
     success_with_data(@response)
 
