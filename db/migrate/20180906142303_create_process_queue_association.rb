68208395fc162b307b8e5004cf5aff7f88615c32
AnaghaMurtarkar <anagha@ost.com>
2018-09-07 15:45:39 +0530

    Migration added for process_queue_association table

---
+++ db/migrate/20180906142303_create_process_queue_association.rb
@@ -0,0 +1,26 @@
+class CreateProcessQueueAssociation < DbMigrationConnection
+
+  def up
+    run_migration_for_db(EstablishSaasConfigDbConnection) do
+      create_table :process_queue_association do |t|
+        t.column :process_id, :integer, null: false
+        t.column :rmq_config_id, :integer, null: false
+        t.column :topic_name, :string, null:false
+        t.column :status, :integer, null: false
+        t.timestamps
+      end
+
+      add_index :process_queue_association, [:process_id, :topic_name], name: 'process_queue_index', unique: true
+    end
+  end
+
+  def down
+    run_migration_for_db(EstablishSaasConfigDbConnection) do
+      remove_index :process_queue_association, name:'process_queue_index'
+      drop_table :process_queue_association
+    end
+  end
+
+end
+
+
