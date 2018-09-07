b58f53a4c5b317172b50db2ff9cfdda69c63e08e
AnaghaMurtarkar <anagha@ost.com>
2018-09-07 18:11:43 +0530

    Migration for composite index on client_id, managed_address_id, process_id

---
+++ db/migrate/20180907122408_add_composite_index_clientid_managed_add_id_processid.rb
@@ -0,0 +1,16 @@
+class AddCompositeIndexClientidManagedAddIdProcessid < DbMigrationConnection
+  def up
+    run_migration_for_db(EstablishSaasClientEconomyDbConnection) do
+      add_index :client_worker_managed_address_ids, [:client_id, :managed_address_id, :process_id], name: 'i_3', unique:true
+    end
+  end
+
+  def down
+    run_migration_for_db(EstablishSaasClientEconomyDbConnection) do
+      remove_index :client_worker_managed_address_ids, name: 'i_3'
+    end
+  end
+end
+
+
+
