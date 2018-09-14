8b8671239922f6e86f1b7dd40ba63158769bd6ac
Shlok Gilda <shlok@ost.com>
2018-09-14 12:37:21 +0530

    Updated migration

--- db/migrate/20180912140939_command_message.rb
+++ db/migrate/20180912140939_command_message.rb
@@ -2,7 +2,7 @@ class CommandMessage < DbMigrationConnection
   def up
     run_migration_for_db(EstablishSaasConfigDbConnection) do
       create_table :command_message do |t|
-        t.column :kind, :string, null: false
+        t.column :kind, :integer, null: false
         t.column :topic_name, :string, null:true
         t.column :status, :integer, null: false
         t.column :extra_data, :string, null: true
