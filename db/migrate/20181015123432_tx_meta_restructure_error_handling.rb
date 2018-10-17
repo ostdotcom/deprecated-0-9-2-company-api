d3fe7225f934b5dd699fb1958bd3f714b968e7e0
ankitsingh <ankit@ost.com>
2018-10-17 18:12:31 +0530

    added column next_retry_timestamp in transaction_meta table

--- db/migrate/20181015123432_tx_meta_restructure_error_handling.rb
+++ db/migrate/20181015123432_tx_meta_restructure_error_handling.rb
@@ -2,9 +2,11 @@ class TxMetaRestructureErrorHandling < DbMigrationConnection
   def up
     completed_status = 0
     default_retry_count = 0
+    default_next_retry_timestamp = 0
     run_migration_for_db(EstablishSaasTransactionDbConnection) do
       add_column :transaction_meta, :status, :integer, after: :client_id, :null => false, default:completed_status
       add_column :transaction_meta, :retry_count, :tinyint, after: :kind, :null => false, default: default_retry_count
+      add_column :transaction_meta, :next_retry_timestamp, :integer, after: :retry_count, :null => false, default: default_next_retry_timestamp
       add_index :transaction_meta, [:transaction_uuid], name: 'uk_tx_uuid', unique: true
       change_column_null :transaction_meta, :transaction_hash, null: true
     end
@@ -14,6 +16,7 @@ class TxMetaRestructureErrorHandling < DbMigrationConnection
     run_migration_for_db(EstablishSaasTransactionDbConnection) do
       change_column_null :transaction_meta, :transaction_hash, null: false
       remove_index :transaction_meta, name: 'uk_tx_uuid'
+      remove_column :transaction_meta, :next_retry_timestamp
       remove_column :transaction_meta, :retry_count
       remove_column :transaction_meta, :status
     end
