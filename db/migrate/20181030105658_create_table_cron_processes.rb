2fddf9523a9ccb77b62881d7a6e7702041323e36
AnaghaMurtarkar <anagha@ost.com>
2018-11-01 16:52:04 +0530

    Added group_id in cron_processes table

--- db/migrate/20181030105658_create_table_cron_processes.rb
+++ db/migrate/20181030105658_create_table_cron_processes.rb
@@ -4,10 +4,11 @@ class CreateTableCronProcesses < DbMigrationConnection
       create_table :cron_processes do |t|
         t.column :kind, :tinyint, limit: 1, null: false
         t.column :ip_address, :string, limit: 255, null: false
+        t.column :group_id, :string, limit: 255, :null => true
         t.column :params, :text, null: true
         t.column :status, :tinyint, limit: 1, null: false
-        t.column :last_start_time, :integer, null: true
-        t.column :last_end_time, :integer, null: true
+        t.column :last_start_time, :datetime, null: true
+        t.column :last_end_time, :datetime, null: true
         t.timestamps
       end
     end
