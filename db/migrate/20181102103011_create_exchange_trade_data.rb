4a1278135605a1c7ed7c1dc82a4d7af53b4634c0
Dhananjay8 <dhananjay@ost.com>
2018-11-05 13:17:59 +0530

    migration related changes for exchange_trades_data table

--- db/migrate/20181102103011_create_exchange_trade_data.rb
+++ db/migrate/20181102103011_create_exchange_trade_data.rb
@@ -2,19 +2,22 @@ class CreateExchangeTradeData < DbMigrationConnection
   def up
     run_migration_for_db(EstablishSaasAnalyticsDbConnection) do
       create_table :exchange_trade_data do |t|
-        t.column :exchange, :tinyint, null: false
-        t.column :trading_pair, :tinyint, null: false
-        t.column :timestamp, :integer, null: false
+        t.column :exchange, :string, null: false
+        t.column :trading_pair, :string, null: false
+        t.column :trade_id, :integer, null: false
+        t.column :timestamp, :integer, limit: 8, null: false
         t.column :price, :decimal, precision: 24, scale: 12, null: false
         t.column :quantity, :decimal, precision: 16, scale: 6, null: false
         t.column :extra_data, :string, null: true
         t.timestamps
       end
+      add_index :exchange_trade_data, [:exchange, :trading_pair, :trade_id], unique: true, name: 'uk_trade_exchange_pair_id_uniq'
     end
   end
   
   def down
     run_migration_for_db(EstablishSaasAnalyticsDbConnection) do
+      remove_index :exchange_trade_data, name: 'uk_trade_exchange_pair_id_uniq'
       drop_table :exchange_trade_data
     end
   end
